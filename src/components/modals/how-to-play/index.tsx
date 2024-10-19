81ca733970b8184819d254ef54f531a48a4d2b81
Jose Ignacio <josemillaquen12@gmail.com>
2024-10-19 18:21:41 -0300

    refactor: update how-to-play content

--- src/components/modals/how-to-play/index.tsx
+++ src/components/modals/how-to-play/index.tsx
@@ -9,47 +9,47 @@ const HowToPlayModal = () => {
       <div className='flex grow flex-col items-center justify-center gap-12 overflow-y-auto overflow-x-hidden p-6 scrollbar-thin md:p-0'>
         <div className='flex flex-col gap-2'>
           <div className='flex flex-col gap-2'>
-            <h2 className='text-2xl font-semibold text-orange-400'>
+            <h2 className='font-creepster text-2xl text-orange-400'>
               How to Play
             </h2>
-            <ul className='list-inside list-disc text-lg *:mb-1 *:text-pretty *:text-muted-foreground'>
+            <ul className='list-inside list-disc text-lg *:text-pretty *:text-muted-foreground [&>*:not(:last-child)]:mb-2'>
               <li>
                 A mysterious Halloween-themed image will gradually reveal itself
                 on the screen. Your goal is to guess the word it represents
                 before the image is fully uncovered.
               </li>
               <li>
-                Tap the ghostly letters below to form your guess. Each letter
-                can only be used once, so choose wisely.
+                Write your guess in the text box at the bottom of the screen.
               </li>
               <li>
                 Once you've formed your guess, summon your courage and click
-                'Submit' to see if you're correct.
+                'Enter' or 'Submit button' to see if you're correct.
               </li>
               <li>
                 If your guess is incorrect, you can try again by rearranging the
                 letters or thinking of a different word.
               </li>
+              <li>
+                If you guess the word, you will be registered as the winner of
+                this game.
+              </li>
             </ul>
           </div>
 
           <div className='flex flex-col gap-2'>
-            <h2 className='text-2xl font-semibold text-orange-400'>Beware!</h2>
-            <ul className='list-inside list-disc text-lg *:mb-1 *:text-pretty *:text-muted-foreground'>
+            <h2 className='font-creepster text-2xl text-orange-400'>Beware!</h2>
+            <ul className='list-inside list-disc text-lg *:text-pretty *:text-muted-foreground [&>*:not(:last-child)]:mb-2'>
               <li>
-                Each <span className='font-bold text-orange-500'>word</span> is
-                conjured by the{' '}
-                <span className='font-bold text-orange-500'>AI</span> from the
-                depths of darkness.
+                Each <b>word</b> is conjured by the <b>AI</b> from the depths of
+                darkness.
               </li>
               <li>
-                The <span className='font-bold text-orange-500'>images</span>{' '}
-                are manifestations of the hidden word's essence.
+                The <b>images</b> are manifestations of the hidden word's
+                essence.
               </li>
               <li>
-                This realm is still in{' '}
-                <span className='font-bold text-orange-500'>beta</span> -
-                spectral repetitions may occur.
+                This realm is still in <b>beta</b> - spectral repetitions may
+                occur.
               </li>
             </ul>
           </div>
